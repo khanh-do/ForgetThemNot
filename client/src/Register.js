0c7863d6db2613ccc4305a78e329353ff1c44e78
cmurph56 <cammurphy195@gmail.com>
2019-11-29 11:07:40 -0600

    included cors

--- client/src/Register.js
+++ client/src/Register.js
@@ -1,10 +1,29 @@
 import React from 'react'
 
 class Register extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { apiResponse : "no resposnse"}
+    }
+
+    callApi() {
+        console.log("registration api called")
+        fetch("http://localhost:8081/register")
+            .then(res => res.text())
+            .then(res => this.setState({ apiResponse : res}))
+            .catch(err => err)
+    }
+    componentDidMount() {
+        this.callApi();
+    }
+
+
     render() {
         return(
             <div class="Registration">
-                <h1> Registration </h1>
+                <h1> testing </h1>
+                <h1> {this.state.apiResponse} </h1>
             </div>
         )
     }
--- server/server.js
+++ server/server.js
@@ -2,14 +2,20 @@
 
 const express = require('express');
 
-// Constants
+var cors = require("cors");
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
+app.use(cors());
+
 app.get('/', (req, res) => {
   res.send('Hello Express\n');
+});
+
+app.get('/register', (req, res) => {
+  res.send('Testing Registration')
 });
 
 app.listen(PORT, HOST);
